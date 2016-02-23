5e959eafe261372b1ecd28551288a36dddd3d9e2
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-22 21:27:47 -0600

    Added Connection Protection Services

---
+++ src/GladLive.Server.Common/Session/IPortToSessionTypeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladLive.Server.Common
+{
+	/// <summary>
+	/// Provides port to <typeparamref name="TSessionType"/> mapping services.
+	/// </summary>
+	/// <typeparam name="TSessionType"></typeparam>
+	public interface IPortToSessionTypeService<TSessionType>
+		where TSessionType : struct, IConvertible
+	{
+		/// <summary>
+		/// Converts a primtive port value to the <typeparamref name="TSessionType"/>.
+		/// </summary>
+		/// <param name="port">Port value.</param>
+		/// <returns>A <see cref="TSessionType"/> mapped from the port value or default if invalid.</returns>
+		TSessionType ToSessionType(int port);
+	}
+}
