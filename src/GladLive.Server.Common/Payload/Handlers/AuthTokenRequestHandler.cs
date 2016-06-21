2d16f52e6e516a66fd07d862d9fb5a6755cf9b11
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-21 01:30:18 -0500

    Add AuthHandlers; Simplified Handler logic

---
+++ src/GladLive.Server.Common/Payload/Handlers/AuthTokenRequestHandler.cs
@@ -0,0 +1,46 @@
+﻿using GladLive.Common;
+using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace GladLive.Server.Common
+{
+	/// <summary>
+	/// Handler for <see cref="AuthenticationRequest"/>s messages for <see cref="IElevatableSession"/> peers.
+	/// </summary>
+	/// <typeparam name="TPeerType">Peer that that implements <see cref="IElevatableSession"/>.</typeparam>
+	[PayloadHandlerType(OperationType.Request, typeof(AuthenticationRequest))]
+	public class AuthenticationRequestHandler<TPeerType> : RequestPayloadHandler<TPeerType, AuthenticationRequest>
+		where TPeerType : IElevatableSession, INetPeer
+	{
+		/// <summary>
+		/// Authentication service the handler defers requests to.
+		/// </summary>
+		private IElevationAuthenticationService elevationAuthService { get; }
+
+		public AuthenticationRequestHandler(ILog logger, IElevationAuthenticationService authService)
+			: base(logger)
+		{
+			elevationAuthService = authService;
+		}
+
+		protected override void HandleNonNullStronglyTypedPayload(AuthenticationRequest payload, IMessageParameters parameters, TPeerType peer)
+		{
+			//TODO: Network the response
+
+			Logger.DebugFormat("{0} session is requesting elevation. ID {1}", peer.GetType(), peer.PeerDetails.ConnectionID);
+
+			//Check if we were sent the expected valid signed message for authentication/elevation
+			if (elevationAuthService.TryAuthenticate(peer, payload.Message))
+			{
+				Logger.DebugFormat("{0} session is now elevated. ID {1}", peer.GetType(), peer.PeerDetails.ConnectionID);
+			}
+			else
+				Logger.WarnFormat("{0} session failed elevation. ID {1}", peer.GetType(), peer.PeerDetails.ConnectionID);
+		}
+	}
+}
---
+++ tests/GladLive.Server.Common.Tests/Payload/Handlers/RequestPayloadHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using GladLive.Common;
+using GladNet.Common;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq.Protected;
+using Common.Logging;
+
+namespace GladLive.Server.Common.Tests
+{
+	[TestFixture]
+	public static class RequestPayloadHandlerTests
+	{
+		[Test]
+		public static void Test_Ctor_Doesnt_Throw()
+		{
+			//assert
+			Assert.DoesNotThrow(() => new RequestPayloadHandlerTest(Mock.Of<ILog>()));
+		}
+
+		[Test]
+		public static void Test_Handler_Returns_False_On_Null_Payload()
+		{
+			//arrange
+			RequestPayloadHandlerTest testHandler = new RequestPayloadHandlerTest(Mock.Of<ILog>());
+
+			//assert
+			Assert.False(testHandler.TryProcessPayload(null, Mock.Of<IMessageParameters>(), Mock.Of<INetPeer>()));
+		}
+
+		[Test]
+		public static void Test_Handler_Calls_Protected_Payload_Handling_Method()
+		{
+			//arrange
+			Mock<RequestPayloadHandlerTest> testHandler = new Mock<RequestPayloadHandlerTest>(MockBehavior.Loose);
+
+			PacketPayload testPayload = Mock.Of<PacketPayload>();
+			IMessageParameters testParameters = Mock.Of<IMessageParameters>();
+			INetPeer testNetPeer = Mock.Of<INetPeer>();
+
+			//act: Call the handler's process method
+			testHandler.Object.TryProcessPayload(testPayload, testParameters, testNetPeer);
+
+			//assert: Check to see if internal was called
+			testHandler.Protected().Verify("HandleNonNullStronglyTypedPayload", Times.Once(), testPayload, testParameters, testNetPeer);
+		}
+
+		public class RequestPayloadHandlerTest : RequestPayloadHandler<INetPeer, PacketPayload>
+		{
+			public RequestPayloadHandlerTest()
+				: base (Mock.Of<ILog>())
+			{
+
+			}
+
+			public RequestPayloadHandlerTest(ILog logger) : base(logger)
+			{
+			}
+
+			protected override void HandleNonNullStronglyTypedPayload(PacketPayload payload, IMessageParameters parameters, INetPeer peer)
+			{
+				
+			}
+		}
+	}
+}
