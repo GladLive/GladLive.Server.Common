f71617eb3aa76612b4e60ae5564eb60069cb719a
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-05 15:38:47 -0500

    Added Elevated Session Handler Attribute

---
+++ src/GladLive.Server.Common/Session/Handlers/ElevatedSessionHandlingRequiredAttribute .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladLive.Server.Common
+{
+	/// <summary>
+	/// Metadata to indcate that a handler requires elevated session verification semantics.
+	/// Sessions that aren't elvated should be unable to reach the handler. Implementation is left to the consumer
+	/// of the libraries.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class ElevatedSessionHandlingRequiredAttribute : Attribute
+	{
+		//We need nothing but the attribute as a metadata marker
+	}
+}
